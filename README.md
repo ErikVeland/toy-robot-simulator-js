815d2a5611ccbc5d32bde2ebe28352091759a1cc
Erik K Veland <veland@Happy-Mac.local>
2018-11-23 12:57:41 +1000

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# toy-robot-simulator-js
-Classic coding test in vanillaJS
+# Mr Roboto - Toy Robot Simulator
+### Classic coding test in vanillaJS
+
+
+## Implementation
+* Vanilla JS
+** No dependencies
+* Cute isometric UI with SVG sprites
+* Automatic reporting
+
+## Future ideas
+* Responsive scaling
+* Arrow keys for movement
+
+## Usage
+
+Open index.html
+
+GUI for control, or issue commands in console:
+- turnLeft();
+- turnRight();
+- move();
+
+// Note: Safari and Firefox need to disable local file restrictions from Develop menu
+
+# Toy Robot Simulator Requirements
+
+Create an application that can read in commands of the following form:
+- PLACE X,Y,F
+- MOVE
+- LEFT
+- RIGHT
+- REPORT
+
+## Requirements
+- The application is a simulation of a toy robot moving on a square tabletop, of dimensions 5 units x 5 units.
+- There are no obstructions on the table surface.
+- The robot is free to roam around the surface of the table, but must be prevented from falling to destruction. Any movement that would result in the robot falling from the table must be prevented, however further valid movement commands must still be allowed.
+- PLACE will put the toy robot on the table in position X,Y and facing NORTH, SOUTH, EAST or WEST.
+- The origin (0,0) can be considered to be the SOUTH WEST most corner.
+- The first valid command to the robot is a PLACE command, after that, any sequence of commands may be issued, in any order, including another PLACE command. The application should discard all commands in the sequence until a valid PLACE command has been executed.
+- MOVE will move the toy robot one unit forward in the direction it is currently facing.
+- LEFT and RIGHT will rotate the robot 90 degrees in the specified direction without changing the position of the robot.
+- REPORT will announce the X,Y and orientation of the robot.
+- A robot that is not on the table can choose to ignore the MOVE, LEFT, RIGHT and REPORT commands.
+- The robot must not fall off the table during movement. This also includes the initial placement of the toy robot. Any move that would cause the robot to fall must be ignored.
+
+##Examples
+
+Example Input and Output:
+a)
+PLACE 0,0,NORTH
+MOVE
+REPORT
+Output: 0,1,NORTH
+b)
+PLACE 0,0,NORTH
+LEFT
+REPORT
+Output: 0,0,WEST
+c)
+PLACE 1,2,EAST
+MOVE
+MOVE
+LEFT
+MOVE
+REPORT
+Output: 3,3,NORTH
+
+## Notes
+- There must be a way to supply the application with input data.
+- Provide test data to exercise the application.
+- The application must run and you should provide sufficient evidence that your solution is complete by, as a minimum, indicating that it works correctly against the supplied test data.
+- The submission should be production quality and it can be done in any language
+- The code should be original and you may not use any external libraries or open source code to solve this problem, but you may use external libraries or tools for building or testing purposes. Specifically, you may use unit testing libraries or build tools available for your chosen language.
---
+++ js/roboto.js
@@ -0,0 +1,129 @@
+// Make sure you disable local file restrictions if viewing in Safari or Firefox 
+
+
+// Declare the globals
+let roboto = document.getElementById('roboto');
+let xPos, yPos, direction;
+
+//Start logging
+let reportLog = document.getElementById("reportLog");
+
+// Set Mr Roboto's start position and direction according to the input values
+// Reporting is automatic, because why the hell not?
+
+function placeRobot() {
+
+    let x = document.getElementById("x");
+    let y = document.getElementById("y");
+    let f = document.getElementById("f");
+    xPos = parseInt((x.options[x.selectedIndex].value));
+    yPos = parseInt((y.options[y.selectedIndex].value));
+    direction = (f.options[f.selectedIndex].text);
+
+    roboto.style.display = "block";
+    roboto.style.left = xPos + "%";
+    roboto.style.top = yPos + "%";
+    roboto.className = direction;
+	report();
+}
+
+//Move Mr Roboto in its current facing direction
+function move() {
+    if (Board()) {
+        if (direction == "North") {
+            if (yPos > 0) {
+                yPos -= 20;
+                roboto.style.top = yPos + "%";
+            }
+
+        } else if (direction == "South") {
+            if (yPos < 80) {
+                yPos += 20;
+                roboto.style.top = yPos + "%";
+
+            }
+        } else if (direction == "East") {
+            if (xPos < 80) {
+                xPos += 20;
+                roboto.style.left = xPos + "%";
+
+
+            }
+        } else if (direction == "West") {
+            if (xPos > 0) {
+                xPos -= 20;
+                roboto.style.left = xPos + "%";
+            }
+        }
+        if ((xPos == 80 && direction == "East") || (yPos == 80 && direction == "South") || (xPos == 0 && direction == "West") || (yPos == 0 && direction == "North")) {
+			console.log("Change direction!");
+			roboto.classList.add("edge");
+
+        } else if ((xPos != 80 && direction == "East") || (yPos != 80 && direction != "South") || (xPos != 0 && direction != "West") || (yPos != 0 && direction != "North")) {
+        	roboto.classList.remove("edge");
+        
+        }
+        if (reportLog.style.visibility == "visible") {
+            reportLog.innerHTML = `PLACE  ${xPos}, ${yPos}, ${direction}`
+        }
+    } else {
+        console.log("Mr Roboto has left the board");
+    }
+	report();
+
+}
+
+//turn to the left
+function turnLeft() {
+    if (Board()) {
+        if (direction == "North") {
+            direction = "West"
+        } else if (direction == "West") {
+            direction = "South"
+        } else if (direction == "South") {
+            direction = "East";
+        } else if (direction == "East") {
+            direction = "North"
+        }
+        roboto.className = direction;
+    } else {
+        console.log("Mr Roboto has left the board");
+    }
+}
+
+//turn to the right
+function turnRight() {
+    if (Board()) {
+        if (direction == "North") {
+            direction = "East";
+
+        } else if (direction == "West") {
+            direction = "North"
+        } else if (direction == "South") {
+            direction = "West"
+        } else if (direction == "East") {
+            direction = "South"
+
+        }
+        roboto.className = direction;
+    } else {
+        console.log("Mr Roboto has left the board");
+    }
+}
+
+//Where is Mr Roboto? 
+function report() {
+	xRealPos = (xPos / 2) / 10;
+	yRealPos = (xPos / 2) / 10;
+    reportLog.innerHTML = `PLACE  ${xRealPos}, ${yRealPos}, ${direction}`
+}
+
+// Is Mr Roboto still on the board?
+function Board() {
+
+    let dir = ["North", "West", "East", "South"];
+
+    if (!dir.includes(direction) || xPos < 0 || xPos > 80 || yPos < 0 || yPos > 80)
+        return false;
+    return true;
+}
